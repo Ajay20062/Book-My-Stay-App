7b248cc840083b102d5797fe272cf8a37d0e4af5
T R Ajay Dharrsan <70842623+Ajay20062@users.noreply.github.com>
2026-03-09 09:34:54 +0530

    Remove main method and update version number
    Updated version number from 1.0 to 0.0 and removed main method.

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -7,22 +7,10 @@
  *
  *</p>
  * @author T R Ajay Dharrsan
- * @version 1.0
+ * @version 0.0
  */
 
 public class BookMyStayApp {
 
-    public static void main(String[] args) {
-
-        System.out.println("=======================================================");
-        System.out.println("               BookMyStayApp App         ");
-        System.out.println("=======================================================");
-        System.out.println();
-        System.out.println("=======================================================");
-        System.out.println("                 Version: 1.0                 ");
-        System.out.println("     Welcome to Book My Stay Application!       ");
-        System.out.println("       Application started successfully.      ");
-        System.out.println("========================================================");
-        System.out.println();
-    }
+    
 }
