642270aafb0245c31df98edc7d39f370ce9fa4f6
Ajay20062 <70842623+Ajay20062@users.noreply.github.com>
2026-03-09 08:39:01 +0530

    Base application setup

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,28 @@
+
+/**
+ * =====================================================
+ * MAIN CLASS - BookMyStayApp
+ * =====================================================
+ * <p>
+ *
+ *</p>
+ * @author T R Ajay Dharrsan
+ * @version 1.0
+ */
+
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=======================================================");
+        System.out.println("               BookMyStayApp App         ");
+        System.out.println("=======================================================");
+        System.out.println();
+        System.out.println("=======================================================");
+        System.out.println("                 Version: 1.0                 ");
+        System.out.println("     Welcome to Book My Stay Application!       ");
+        System.out.println("       Application started successfully.      ");
+        System.out.println("========================================================");
+        System.out.println();
+    }
+}
