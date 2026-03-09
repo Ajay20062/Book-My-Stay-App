aceeb860657ab8b25331b74c916aa661416590b6
Ajay20062 <70842623+Ajay20062@users.noreply.github.com>
2026-03-09 08:42:51 +0530

    UC1: Application Entry & Welcome Message

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -4,7 +4,7 @@
  * MAIN CLASS - BookMyStayApp
  * =====================================================
  * <p>
- *
+ *     Establish a clear and predictable starting point for the Hotel Booking application by demonstrating how a Java program begins execution and produces console output.
  *</p>
  * @author T R Ajay Dharrsan
  * @version 1.0
@@ -15,13 +15,13 @@ public class BookMyStayApp {
     public static void main(String[] args) {
 
         System.out.println("=======================================================");
-        System.out.println("               BookMyStayApp App         ");
+        System.out.println("                       BookMyStayApp App               ");
         System.out.println("=======================================================");
         System.out.println();
         System.out.println("=======================================================");
-        System.out.println("                 Version: 1.0                 ");
-        System.out.println("     Welcome to Book My Stay Application!       ");
-        System.out.println("       Application started successfully.      ");
+        System.out.println("                      Version: 1.0                     ");
+        System.out.println("         Welcome to Book My Stay Application!          ");
+        System.out.println("            Application started successfully.          ");
         System.out.println("========================================================");
         System.out.println();
     }
